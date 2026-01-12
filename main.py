38ae016270772df1fc18758d93226f2a5a9bbcb8
cmorenoup-gif <cmoreno.up@gmail.com>
2026-01-12 19:14:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main_endpoint():
     # GET → prueba real de Amazon US SP-API
     try:
         sellers = Sellers(marketplace=Marketplaces.US)
-        response = get_marketplace_participations()
+        response = sellers.get_marketplace_participations()
         return jsonify(response.payload)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
