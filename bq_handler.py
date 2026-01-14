b2930450feb57979da30e87ce22bec10202bb26a
cmorenoup-gif <cmoreno.up@gmail.com>
2026-01-14 04:27:58 +0100

    Create bq_handler.py

---
+++ bq_handler.py
@@ -0,0 +1,24 @@
+from google.cloud import bigquery
+import logging
+
+PROJECT_ID = "amazon-cornilove"
+DATASET_ID = "amz_cornilove"
+
+def auto_insert_to_bq(data, table_id):
+    """Inserta datos y crea la tabla automáticamente si no existe"""
+    client = bigquery.Client(project=PROJECT_ID)
+    table_ref = f"{PROJECT_ID}.{DATASET_ID}.{table_id}"
+    
+    job_config = bigquery.LoadJobConfig(
+        autodetect=True,
+        write_disposition="WRITE_APPEND",
+    )
+
+    if data:
+        try:
+            load_job = client.load_table_from_json(data, table_ref, job_config=job_config)
+            load_job.result()
+            return None
+        except Exception as e:
+            return str(e)
+    return "No hay datos para procesar"
